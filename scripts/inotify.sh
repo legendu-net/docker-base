f6efe514c1d6a861a66d8d01b907d87e784d342b
Benjamin Du <duchuanlong+git@gmail.com>
2018-07-21 15:31:52 +0800

    update inotify.sh

--- scripts/inotify.sh
+++ scripts/inotify.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-dir=/jupyter
+dir=/workdir
 inotifywait -m -r -e create --format '%w%f' "$dir" | while read f; do
-#    if [[ "$f" == */.ipynb_checkpoints ]]; then
     if [[ -d "$f" ]]; then
         chmod 775 "$f"
     fi
