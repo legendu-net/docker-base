868fefd110deffaf50977e62e8a48741a494c671
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-09-08 17:01:57 -0700

    Update purge_cache.sh

--- scripts/sys/purge_cache.sh
+++ scripts/sys/purge_cache.sh
@@ -11,6 +11,11 @@ if [[ "$(which python3.7)" != "" ]]; then
     python3.7 -m pip cache purge -qqq
 fi
 
+# purge npm cache 
+if [[ "$(which python)" != "" ]]; then
+    npm cache clean --force
+fi
+
 # purge apt-get cache
 if [[ "$(which apt-get)" != "" ]]; then
     apt-get autoremove -y
