d3880bf360b7e36e8cd1ad2c0ee588ffd30611f4
Ben Chuanlong Du <dclong@users.noreply.github.com>
2022-06-27 15:38:39 -0700

    Update path.sh

--- scripts/path.sh
+++ scripts/path.sh
@@ -2,7 +2,7 @@
 
 # set $PATH
 _PATHS=(
-    $(ls -d $HOME/.*/bin 2> /dev/null)
+    $(ls -d $HOME/*/bin 2> /dev/null)
     $(ls -d $HOME/Library/Python/3.*/bin 2> /dev/null)
 )
 for ((_i=${#_PATHS[@]}-1; _i>=0; _i--)); do
