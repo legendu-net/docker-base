cac45169185e1fa41c353f3f072caa7823450ca0
Ben Chuanlong Du <dclong@users.noreply.github.com>
2023-04-26 21:25:28 -0700

    Update create_user.sh

--- scripts/sys/create_user.sh
+++ scripts/sys/create_user.sh
@@ -33,7 +33,7 @@ function create_user() {
     fi
     local user=$1
     local user_id=$2
-    local password=$3
+    local password="$3"
     local group=$4
     local group_id=$5
     if [[ $user == "" || $user_id == "" || $password == "" || $group == "" || $group_id == "" ]]; then 
